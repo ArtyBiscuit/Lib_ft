d2afb4054e851567426fce0ad395d63ba6bc4d6b
ArtyBiscuit <forgeard.arthur@hotmail.com>
2022-10-05 06:21:26 +0200

    ft_itoa

--- libft.h
+++ libft.h
@@ -47,5 +47,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_itoa(int n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 #endif  
